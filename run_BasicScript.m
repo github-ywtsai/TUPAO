67907ec4e5cdfdd9bd62f4f5e5d8288bc2536e63
tsai.yw <tsai.yw@DGX1>
2023-02-14 14:53:06 +0800

    new script including probe normalization

--- run_BasicScript.m
+++ run_BasicScript.m
@@ -4,6 +4,7 @@ mask_info = gen_mask_info(init_cond);
 measurement_info = gen_measurement_info(init_cond,mask_info);
 object_info = gen_object_info(init_cond);
 probe_info = gen_probe_info(init_cond,'config_probe.txt');
+probe_info = normalize_probe(probe_info,measurement_info);
 iteration_para = gen_iteration_para(init_cond,measurement_info,object_info,probe_info,'config_iteration.txt');
 
-ptycho(init_cond,mask_info,measurement_info,object_info,probe_info,iteration_para)
+[object_info, probe_info, iteration_para] =  ptycho(init_cond,mask_info,measurement_info,object_info,probe_info,iteration_para);
